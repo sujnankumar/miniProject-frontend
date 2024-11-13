0f3ee42ecda3d98d173f311141fc5c819a6f607b
sujnankumar <sujnankumar439@gmail.com>
2024-11-13 14:34:20 +0530

    error fix

--- src/App.js
+++ src/App.js
@@ -6,15 +6,10 @@ import Chatbot from './components/Chat/Chatbot';
 import FoodInfo from './components/Chat/FoodInfo';
 import UserProfile from './components/UserProfile';
 import Layout from './components/Layout';
-<<<<<<< HEAD
-import RestaurantProfile from './components/Restaurant/RestaurantInfo';
-import RestaurantDashboard from './components/Restaurant/RestaurantDashboard';
-import RestaurantRegister from './components/Restaurant/RestaurantRegister';
-=======
+import RestaurantRegister from './components/RestaurantDashboard/RestaurantRegister';
 import RestaurantProfile from './components/RestaurantDashboard/RestaurantInfo';
 import RestaurantDashboard from './components/RestaurantDashboard/RestaurantDashboard';
 import AddDishForm from './components/RestaurantForms/AddDishForm';
->>>>>>> 5c81b98c929aea74a33318eba18d902d7287d031
 
 function App() {
   return (
@@ -30,12 +25,9 @@ function App() {
           <Route path="/order" element={<Order />} />
           <Route path='/dish-details' element={<FoodInfo />} />
           <Route path="/profile" element={<UserProfile />} />
-<<<<<<< HEAD
 
           <Route path='/restaurant/register' element={<RestaurantRegister />} />
-=======
           <Route path="/restaurant/adddish" element={<AddDishForm />} />
->>>>>>> 5c81b98c929aea74a33318eba18d902d7287d031
         </Routes>
       </div>
       </Layout>
